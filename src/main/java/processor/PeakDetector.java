b999bc240aa4dca11e64b705ae324ad95699c873
CrushedPixel <crushedpixelmaps@gmail.com>
2016-07-14 19:04:30 +0200

    Remove unused method PeakDetector#sortByValue

--- src/main/java/processor/PeakDetector.java
+++ src/main/java/processor/PeakDetector.java
@@ -13,13 +13,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 public class PeakDetector extends AudioProcessor {
 
@@ -104,22 +98,6 @@ public class PeakDetector extends AudioProcessor {
         double avgDifference = sum/total;
 
         return 1/avgDifference * 60;
-    }
-
-    private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
-        List<Map.Entry<K, V>> list =
-                new LinkedList<Map.Entry<K, V>>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
-            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
-                return (o2.getValue()).compareTo(o1.getValue());
-            }
-        });
-
-        Map<K, V> result = new LinkedHashMap<K, V>();
-        for (Map.Entry<K, V> entry : list) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
     }
 
     private EventList findBeats() {
